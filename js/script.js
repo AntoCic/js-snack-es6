e6659b7cf5206a3fb6e4ce20a48577eb8b64c973
AntoCic <anto.cic.127@gmail.com>
2024-03-19 18:01:57 +0100

    SNACK 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 console.log("- START -");
+// SNACK 1
 const tavoloVip = [
   "Brad Pitt",
   "Johnny Depp",
@@ -20,3 +21,63 @@ tavoloVip.forEach((e, index) => {
   });
 });
 console.log(tavoloVipIterated);
+
+// SNACK 2
+const students = [
+    {
+        Id: 213,
+        Name: "Marco della Rovere",
+        Grades: 78,
+    },
+    {
+        Id: 110,
+        Name: "Paola Cortellessa",
+        Grades: 96,
+    },
+    {
+        Id: 250,
+        Name: "Andrea Mantegna",
+        Grades: 48,
+    },
+    {
+        Id: 145,
+        Name: "Gaia Borromini",
+        Grades: 74,
+    },
+    {
+        Id: 196,
+        Name: "Luigi Grimaldello",
+        Grades: 68,
+    },
+    {
+        Id: 102,
+        Name: "Piero della Francesca" ,
+        Grades: 50,
+    },
+    {
+        Id: 120,
+        Name: "Francesca da Polenta",
+        Grades: 84,
+    },
+];
+
+const list1 = students.map((e)=>{
+    return e.Name.toUpperCase();
+})
+console.log(list1);
+
+const list2 = students.filter((e)=>{
+    return e.Grades > 70;
+})
+console.log(list2);
+
+const list3 = students.filter((e)=>{
+    return e.Grades > 70 && e.Id > 120;
+})
+console.log(list3);
+         
+   	      
+            
+   	 
+    
+      
