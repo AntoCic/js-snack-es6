76f270ee40721ae7b547c78de0f653f43e65f930
AntoCic <anto.cic.127@gmail.com>
2024-03-19 17:46:29 +0100

    SNACK 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("- START -");
+const tavoloVip = [
+  "Brad Pitt",
+  "Johnny Depp",
+  "Lady Gaga",
+  "Cristiano Ronaldo",
+  "Georgina Rodriguez",
+  "Chiara Ferragni",
+  "Fedez",
+  "George Clooney",
+  "Amal Clooney",
+  "Maneskin",
+];
+let tavoloVipIterated = [];
+tavoloVip.forEach((e, index) => {
+  tavoloVipIterated.push({
+    nomeTavolo: "Tavolo VIP",
+    nomeOspite: e,
+    posto: index + 1,
+  });
+});
+console.log(tavoloVipIterated);
